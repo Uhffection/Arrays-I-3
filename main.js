25509672acdb5959a560d513bfd6cd0721aa3787
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-06-10 23:11:34 +0000

    Update 'fruits' array changing index 0, 2, and adding a 6th index item / 7th element

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 var fruits = ["apple", "banana", "cherry", "dragon fruit", "elderberry", "fig"]; 
 
 // Write your code below
+fruits[0] = "apricot";
+
+fruits[2] = "clementine";
+
+fruits[6] = "grape";
+
+console.log(fruits)
